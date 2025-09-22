7bb8dabf895c917c806fccacef9de0719429f0b9
Ashutosh Kumar <ashutosh2003.sahi@gmail.com>
2025-09-22 20:19:45 +0530

    add product model with schema for managing product details and inventory

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema(
+	{
+		sellerId: {
+			type: Schema.Types.ObjectId,
+			ref: "Seller",
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		originalPrice: {
+			type: Number,
+			required: true,
+		},
+		offeredPrice: {
+			type: Number,
+			required: true,
+		},
+		stock: {
+			type: Number,
+			required: true,
+			min: 0,
+		},
+		colors: [{ type: String }],
+		categoryId: {
+			type: Schema.Types.ObjectId,
+			ref: "Category",
+			required: true,
+		},
+		productImages: [
+			{
+				type: String,
+				required: true,
+			},
+		],
+	},
+	{ timestamps: true }
+);
