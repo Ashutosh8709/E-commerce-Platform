433f179c55ffb709c83f83f373fd6bccff44f1d3
Ashutosh Kumar <ashutosh2003.sahi@gmail.com>
2025-11-23 09:50:19 +0530

    feat: configure Redis client with host and port settings; update docker-compose for Redis service

--- backend/src/client.js
+++ backend/src/client.js
@@ -1,6 +1,9 @@
 import { Redis } from "ioredis";
 
-const client = new Redis();
+const client = new Redis({
+  host: "redis",
+  port: 6379,
+});
 
 client.on("connect", () => console.log("Redis connected"));
 client.on("error", (err) => console.error("Redis error", err));
